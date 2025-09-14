06052901433aa10d2a356613ee0a9cd30b2dc6b5
thumatimanasa <161281232+thumatimanasa@users.noreply.github.com>
2025-09-14 21:25:13 +0530

    Add files via upload

---
+++ functions/lambda_function.py
@@ -0,0 +1,22 @@
+
+#Example 1
+def add(a,b):
+    return a+b
+print(add(5,6))
+
+#using lambda function
+add=lambda a,b:a+b
+print(add(3,4))
+
+#Example 2
+def even(num):
+    if num%2==0:
+        return True
+print(even(24))
+
+#using lambda function
+even=lambda num:num%2==0
+print(even(34))
+
+
+
